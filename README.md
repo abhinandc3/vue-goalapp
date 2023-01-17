3837f70e5913d7b884170833e018a5b270c83a58
abhinandc3 <98801778+abhinandc3@users.noreply.github.com>
2023-01-17 14:51:01 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-goalapp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-qqvtgv)
---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+  <h1>goal</h1>
+   <input type="text" v-model="enteredValue"></input>
+   <button @click="addGoal()"> add goals
+      </button>
+   <ul>
+   <li v-for = "goal in goals">{{goal}}
+   </li > 
+   </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+      return {
+        goals: [],
+        enteredValue: ''
+      }},
+
+  methods : 
+  {
+    addGoal(){
+      this.goals.push(this.enteredValue)
+      this.enteredValue=""
+    }
+
+  }
+  }
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
